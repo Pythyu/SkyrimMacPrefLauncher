7d00bb0d034100fdf3e5a804e3cc1966044db2fb
Pythyu <famille@imac-jjr.home>
2020-07-22 13:41:32 +0200

    Cleanup

--- src/appfiles/setup.py
+++ src/appfiles/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(
-    app=["main.py"],
+    app=["app.py"],
     options={'py2app':{'argv_emulation':True,'emulate_shell_environment':1}},
     setup_requires=["py2app"],
 )
